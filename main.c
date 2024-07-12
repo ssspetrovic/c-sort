4137b6c54393dace7ad83d4ba4f7419295339f52
Srdjan <72706620+ssspetrovic@users.noreply.github.com>
2024-07-12 17:44:51 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "sort.h"
 
+#define ARRAY_LEN 6
+
 int main()
 {
     int arr[6] = { 5, 2, 4, 6, 1, 3 };
-    insertion_sort(arr, 6);
+    insertion_sort(arr, ARRAY_LEN);
 
-    for (int i = 0; i < 6; ++i)
+    for (int i = 0; i < ARRAY_LEN; ++i)
         printf("%d ", arr[i]);
 
     return 0;
